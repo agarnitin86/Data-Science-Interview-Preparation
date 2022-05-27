338e3a181b13b921ee8df836ec46c32a001e6ce2
Nitin Agarwal <agar.nitin86@gmail.com>
2022-05-28 02:06:06 +0530

    Added Readme

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-cat generic-ml.md regression.md classification.md knn.md svm.md trees.md model-finetuning.md evaluation.md pca.md deep-learning.md statistics.md eda.md timeseries.md case-study.md dp.md > README_1.md
+cat generic-ml.md regression.md classification.md knn.md svm.md trees.md model-finetuning.md evaluation.md pca.md deep-learning.md statistics.md eda.md timeseries.md case-study.md dp.md > README.md
